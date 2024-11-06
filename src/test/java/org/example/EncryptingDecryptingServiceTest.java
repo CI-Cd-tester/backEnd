82dd6df77f48d09d0ba517804a070c73a43734e4
Nasser Amer <nasser_amer09@yahoo.com>
2024-11-06 15:58:26 +0100

    codded more  unit test to test different secretKey

--- src/test/java/org/example/EncryptingDecryptingServiceTest.java
+++ src/test/java/org/example/EncryptingDecryptingServiceTest.java
@@ -37,7 +37,19 @@ class EncryptingDecryptingServiceTest {
     String testText = "_*'.!@!%∞4n/";
     String encryptedText = encryptingDecryptingService.encrypt(testText);
     String decryptedText = encryptingDecryptingService.decrypt(encryptedText);
-    
+
     assertEquals(testText, decryptedText);
+  }
+
+  @Test
+  void differentKeyTest() throws Exception {
+
+    String testText = "This is a test text to test encrypting and decrypting with different key";
+    String encryptedText = encryptingDecryptingService.encrypt(testText);
+    EncryptingDecryptingService newService= new EncryptingDecryptingService();
+    Exception exception = assertThrows(Exception.class,() ->
+            newService.decrypt(encryptedText));
+
+    assertEquals(exception.getMessage(),testText);
   }
 }
